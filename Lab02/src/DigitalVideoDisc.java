edab203cf46087319063c233ec7660deb1add747
Yuuki <bachbui1022@gmail.com>
2026-04-03 17:56:53 +0700

    commit lab 02

---
+++ Lab02/src/DigitalVideoDisc.java
@@ -0,0 +1,55 @@
+public class DigitalVideoDisc {
+    // Part 6
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    // Part 7
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    // Part 8
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+}
